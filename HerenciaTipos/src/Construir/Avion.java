c21f3a36afa568ea4edc320f20076a7ea15b11d0
hanniahaydar <148922406+hanniahaydar@users.noreply.github.com>
2023-11-21 22:27:06 -0600

    modificacion en la clase avion

--- HerenciaTipos/src/Construir/Avion.java
+++ HerenciaTipos/src/Construir/Avion.java
@@ -1,27 +1,38 @@
 package Construir;
 
-publc class Avion {
+public class Avion {
 
-public void Descender(){
-    System.out.println("Esta decendiendo");
-}
- public void setAlas(int alas){
-     this.alas=alas;
-
- }
-public int getAlas(){
-    return alas;
-}
-public int getRuedas(){
-    return ruedas;
-}
-public int getAltura(){
-    return altura;
-}
-
-public boolean getCola(){
-    return cola;
-    
-}
-
+	  public int alas;
+	    private int altura;
+	    private int ruedas;
+	    private boolean cola;
+	    
+	    
+	    public void Volar(){
+	        System.out.println("Esta volando");
+	    }
+	    
+	    
+	    public void Descender(){
+	        System.out.println("Esta decendiendo");
+	    }
+	     public void setAlas(int alas){
+	         this.alas=alas;
+	    
+	     }
+	    public int getAlas(){
+	        return alas;
+	    }
+	    public int getRuedas(){
+	        return ruedas;
+	    }
+	    public int getAltura(){
+	        return altura;
+	    }
+	    
+	    public boolean getCola(){
+	        return cola;
+	        
+	    }
+	    
 }
