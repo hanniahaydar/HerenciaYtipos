299c2c124340bfcdabf50249248df2af9607799c
hanniahaydar <148922406+hanniahaydar@users.noreply.github.com>
2023-11-21 19:38:06 -0600

    clases extends de seleccionfutbol

---
+++ HerenciaTipos/src/Extender/Medico.java
@@ -0,0 +1,13 @@
+package Extender;
+
+public class Medico extends SeleccionFutbol {
+
+	   private String especialidad;
+		
+	   public void segumientoDeSalud() {
+	      System.out.println("Auxiliar al herido");
+	   }
+	   public void darConsultaMedica(){
+	     System.out.println("Da consulta médica");
+	     }
+	}
---
+++ HerenciaTipos/src/Extender/Nutriologo.java
@@ -0,0 +1,12 @@
+package Extender;
+
+public class Nutriologo extends SeleccionFutbol {
+    
+    private String cedulaProfesional;
+    
+    public void darConsulta(){
+        System.out.println("Mostrar plan alimenticio");
+}
+
+
+}
