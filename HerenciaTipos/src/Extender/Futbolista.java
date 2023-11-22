99cf9c2061536aa4a1e6259a175d72d1feda9a21
hanniahaydar <148922406+hanniahaydar@users.noreply.github.com>
2023-11-21 19:36:33 -0600

    clases masajista y futbolista

---
+++ HerenciaTipos/src/Extender/Futbolista.java
@@ -0,0 +1,17 @@
+package Extender;
+
+public class Futbolista extends SeleccionFutbol
+{
+	private int dorsal;
+	private String demarcacion;
+
+        public Futbolista() {
+		super();
+	}
+
+	public void jugarPartido() {
+	}
+
+	public void entrenar() {
+	}
+}
---
+++ HerenciaTipos/src/Extender/Masajista.java
@@ -0,0 +1,15 @@
+package Extender;
+
+public class Masajista extends SeleccionFutbol {
+
+	   private String titulacion;
+	   private int aniosExperiencia;
+		
+	   public void entrenamiento() {
+	      System.out.println("Da asistencia en el entrenamiento (Clase Masajista)");
+	   }
+
+	   public void darMasaje() {
+	      System.out.println("Da un Masaje");
+	   }
+	}
