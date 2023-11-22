b08f4addbed160c19632f51cd336b0fba0072262
hanniahaydar <148922406+hanniahaydar@users.noreply.github.com>
2023-11-21 19:41:22 -0600

    clases extends añadidas

---
+++ HerenciaTipos/src/Extender/Arbitro.java
@@ -0,0 +1,14 @@
+package Extender;
+
+public class Arbitro extends SeleccionFutbol {
+
+	   private String Tarjetas;
+
+	   public void partidoFutbol() {
+	      System.out.println("Supervisar partido");
+	   }
+
+	   public void sacarTarjeta() {
+	      System.out.println("Saca tarjeta roja a futbolista");
+	   }
+	}
---
+++ HerenciaTipos/src/Extender/Patrocinador.java
@@ -0,0 +1,10 @@
+package Extender;
+
+public class Patrocinador extends SeleccionFutbol {
+    
+    private String marca;
+    
+    public void patrocinar(){
+         System.out.println("Ofrecer patrocinio");
+    }
+}
