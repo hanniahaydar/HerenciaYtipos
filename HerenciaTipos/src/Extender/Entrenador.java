7471c559a3ee4c4206c886dbedc2ba97ad024e38
hanniahaydar <148922406+hanniahaydar@users.noreply.github.com>
2023-11-21 19:35:24 -0600

    nuevas clases de extends

---
+++ HerenciaTipos/src/Extender/Entrenador.java
@@ -0,0 +1,17 @@
+package Extender;
+
+public class Entrenador extends SeleccionFutbol
+{
+
+	private String idFederacion;
+
+        public Entrenador() {
+		super();
+	}
+
+	public void dirigirPartido() {
+	}
+
+	public void dirigirEntreno() {
+	}
+}
--- HerenciaTipos/src/Extender/SeleccionFutbol.java
+++ HerenciaTipos/src/Extender/SeleccionFutbol.java
@@ -1,5 +1,17 @@
 package Extender;
+public class SeleccionFutbol{
 
-public class SeleccionFutbol {
+	protected int id;
+	protected String Nombre;
+	protected String Apellidos;
+	protected int Edad;
 
+	public void concentrarse() {
+	}
+
+	public void viajar() {
+	}
+	
+	public void verPartido(){
+	}
 }
